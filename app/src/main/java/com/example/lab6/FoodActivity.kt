ea3abda4e04f173e197bd9e8e772583ea9cf8276
shabardin.i <igorshabardin42@gmail.com>
2024-04-05 20:57:47 +0300

    i

---
+++ app/src/main/java/com/example/lab6/FoodActivity.kt
@@ -0,0 +1,14 @@
+package com.example.lab6
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class FoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_food)
+
+        // Здесь вы можете установить адаптер для recyclerView с данными о еде
+    }
+}
